b2730d1852ada6d391e29d1c9e3a6a039a0a1831
Cameron Mills <19873476+millsjc@users.noreply.github.com>
2022-10-06 20:31:42 +0200

    Update fake_o3.sh

--- test_hm/fake_o3.sh
+++ test_hm/fake_o3.sh
@@ -1,4 +1,5 @@
-pycbc_multi_inspiral_hm --fake-strain "zeroNoise" --allow-zero-padding \
+pycbc_multi_inspiral_hm --fake-strain 'zeroNoise' \
+--allow-zero-padding \
 --injection-file H1:injection.xml L1:injection.xml V1:injection.xml \
 --psd-model H1:aLIGOaLIGOO3LowT1800545 L1:aLIGOaLIGOO3LowT1800545 V1:aLIGOAdVO3LowT1800545 \
 --channel-name H1:DCH-CLEAN_STRAIN_C02 L1:DCH-CLEAN_STRAIN_C02 V1:Hrec_hoft_16384Hz  \
@@ -7,12 +8,17 @@ pycbc_multi_inspiral_hm --fake-strain "zeroNoise" --allow-zero-padding \
 --segment-length 16 --segment-start-pad 4 \
 --segment-end-pad 2 --allow-zero-padding  --taper-data 1 \
 --low-frequency-cutoff 20 \
+--timing-error 0.005 \
 --approximant 'IMRPhenomHM:mtotal<4' 'IMRPhenomHM:else' \
 --order -1 --snr-threshold 4. --cluster-window 0 \
 --processing-scheme cpu \
 --output TESTING-512.hdf \
 --instruments H1 L1 V1 \
 --bank-file GW190521_template.hdf \
---coinc-threshold 7.0 --ra 0.644911865031637 \
---dec 0.810795263791696 --trigger-time 1187008882 \
---verbose
+--coinc-threshold 7.0 --verbose 
+
+#--ra 0.644911865031637 \
+#--dec 0.810795263791696 --trigger-time 1187008882 \
+#--gps-start-time 1187008374 --gps-end-time 1187008886 \
+#1187008860 --gps-end-time 1187008886
+#H1:aLIGOaLIGOO3LowT1800545 L1:aLIGOaLIGOO3LowT1800545 V1:aLIGOAdVO3LowT1800545
