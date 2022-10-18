2efe49470afa4c0e835c39f63324ad4798ce2e85
Cameron Mills <19873476+millsjc@users.noreply.github.com>
2022-10-18 20:05:32 +0200

    Update create_injection.sh

--- test_hm/create_injection.sh
+++ test_hm/create_injection.sh
@@ -6,5 +6,5 @@
 
 #source _inject.sh 150 30 100000 'IMRPhenomHMpseudoFourPN' 0 0 90 1187008882 0 0 20
 #overhead Hanford:
-source _inject.sh 150 30 100000 'IMRPhenomHMpseudoFourPN' 36.95072802 46.45514666 90 1187008882 0 0 20
+source _inject.sh 150 30 600000 'IMRPhenomHMpseudoFourPN' 36.95072802 46.45514666 90 1187008882 0 0 20
 #source _inject.sh 1.457423 1.299302 30000 'IMRPhenomD' 0 0 0 1187008882 0 0 20
