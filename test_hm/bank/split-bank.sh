9e2f46b722c11adb81aee93d315468318b3c59b3
Cameron Mills <19873476+millsjc@users.noreply.github.com>
2022-11-01 16:46:35 +0100

    split bank

---
+++ test_hm/bank/split-bank.sh
@@ -0,0 +1,1 @@
+/Users/camill/projects/pycbc/bin/hm/pycbc_hm_splitbank --inputfile /Users/camill/projects/pycbc/test_hm/bank/full-correct-tau-xhm-spinning-2filter-listharms.hdf --outdir ./bank --num-splits 200
