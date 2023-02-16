35114fd70c2fe5ae321416c13cad1c5f4577629a
Cameron Mills <19873476+millsjc@users.noreply.github.com>
2023-02-16 16:59:52 +0100

    Update create_injections.sh

--- test_hm/inj/create_injections.sh
+++ test_hm/inj/create_injections.sh
@@ -1,9 +1,11 @@
-OUTFILE='mini-bbh-3dets-5mins.hdf'
-GPS_START=1267736168
+OUTFILE='mini-bbh-3dets-512s.hdf'
+PAD=2
+GPS_START=$((1267735956-${PAD}))
+GPS_END=$((1267736468-${PAD}))
 INJ_FILE=$1
 pycbc_create_injections \
 --gps-start-time ${GPS_START} \
---gps-end-time 1267736468 \
+--gps-end-time ${GPS_END} \
 --time-step 16 --time-window 5 \
 --seed 1234 \
 --output-file $OUTFILE --force --verbose --config-files $INJ_FILE
