20fd38ce012d7289e0fa0d95bbf437c66e95f01e
Cameron Mills <19873476+millsjc@users.noreply.github.com>
2023-02-16 17:01:16 +0100

    Update real_o3.sh

--- test_hm/real_o3.sh
+++ test_hm/real_o3.sh
@@ -1,8 +1,10 @@
-INJFILE='mini-bbh-3dets-5mins.hdf'
+OUTFILE='MINI-TESTING-512-realdata-INJECTIONS-timeslides.hdf'
+INJFILE='mini-bbh-3dets-512s.hdf'
 #GPS_START=1267732868
 #GPS_START=1187008374
-GPS_START=1267735956
-OUTFILE='MINI-TESTING-512-5mins-realdata.hdf'
+PAD=2
+GPS_START=$((1267735956-${PAD}))
+GPS_END=$((1267736468-${PAD}))
 #BANKFILE='full-correct-tau-xhm-spinning-2filter-listharms.hdf'
 #BANKFILE='splitbank/full-correct-tau-xhm-spinning-2filter-listharms_SPLITBANK_0.hdf'
 BANKFILE='mini-bank.hdf'
@@ -12,17 +14,21 @@ v1chan=V1:GWOSC-4KHZ_R1_STRAIN
 h1frame=H1_GWOSC_O3b_4KHZ_R1
 l1frame=L1_GWOSC_O3b_4KHZ_R1
 v1frame=V1_GWOSC_O3b_4KHZ_R1
-WD=/work/cameron.mills/projects/pycbc/test_hm
-#WD=/Users/camill/projects/pycbc/test_hm
+# FRAME_COMMAND="--frame-type H1:${h1frame} L1:${l1frame} V1:${v1frame}"
+FRAME_COMMAND="--frame-files H1:./strain/H-${h1frame}-1267732480-4096.gwf L1:./strain/L-${l1frame}-1267732480-4096.gwf  V1:./strain/V-${v1frame}-1267732480-4096.gwf "
+# WD=/work/cameron.mills/projects/pycbc/test_hm
+WD=/Users/camill/projects/pycbc/test_hm
 #EXEC='python -m pdb -c continue /work/cameron.mills/miniconda3/envs/igwn-py38/bin/pycbc_multi_inspiral_hm'
 EXEC='pycbc_multi_inspiral_hm'
+
 
 $EXEC \
 --allow-zero-padding \
 --psd-estimation median --psd-segment-length 16 --psd-segment-stride 8 --psd-inverse-length 16 --psd-num-segments 63 \
 --channel-name H1:${h1chan} L1:${l1chan} V1:${v1chan}  \
---frame-type H1:${h1frame} L1:${l1frame} V1:${v1frame} \
---gps-start-time $GPS_START --gps-end-time 1267736468 \
+$FRAME_COMMAND \
+--gps-start-time $GPS_START --gps-end-time $GPS_END \
+--pad-data $PAD \
 --strain-high-pass 15 --sample-rate 512 \
 --segment-length 512 --segment-start-pad 4 \
 --segment-end-pad 2 --allow-zero-padding  --taper-data 1 \
@@ -34,9 +40,9 @@ $EXEC \
 --output $WD/inspiral/$OUTFILE \
 --bank-file $WD/bank/$BANKFILE \
 --instruments H1 L1 V1 \
---coinc-threshold 7.0 --verbose --num-timeslides 0
+--coinc-threshold 7.0 --verbose --num-timeslides 1 \
+--injection-file $WD/inj/$INJFILE \
 
-#--injection-file $WD/inj/$INJFILE \
 #--psd-model H1:aLIGOaLIGOO3LowT1800545 L1:aLIGOaLIGOO3LowT1800545 V1:aLIGOAdVO3LowT1800545 \
 # --fake-strain 'zeroNoise' \
 #--bank-file bank/full-correct-tau-xhm-spinning-2filter-listharms.hdf \
